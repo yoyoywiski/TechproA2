f3293161c69706c6529867f477b8beffec641a08
yoyoywiski <cascassemirtenedero.scc@gmail.com>
2025-08-27 13:00:50 +0000

    done adding output

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+      int countdown = 3;
+          System.out.println("Seatworkfive");
 
+              while (countdown > 0) {
+                    System.out.println(countdown);
+                          countdown--;
+                              }
+                                  
+                                      System.out.println("Happy Birthday!!");
+                                        }
+                                        }
